292c47f7ea1da12367f65644837c0051d4f738a4
venkat2022p <venkatvenky0151@gmail.com>
2024-03-29 12:28:54 +0530

    test

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -8,20 +8,30 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
-  title!: string;
-  description!: string;
-  price!: string;
-  image!: string;
+
+  f: any
+  submitted : boolean = false
+  // title!: string;
+  // description!: string;
+  // price!: string;
+  // image!: string;
+  form ={
+    title:'',
+    description:'',
+    price:'',
+    image:''
+  }
   constructor(private productservice: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  submit() {
-    if (this.title == "" || this.description == "" || this.price == "" || this.image == "") {
+  onSubmit() {
+    this.submitted = true
+    if (this.form.title == "" || this.form.description == "" || this.form.price == "" || this.form.image == "") {
       alert('All Fileds are Mondatory');
       return false
     }
-    var product = { id: 0, title: this.title, description: this.description, price: this.price, image: this.image }
+    var product = { id: 0, title: this.form.title, description: this.form.description, price: this.form.price, image: this.form.image }
     this.productservice.createProducts(product).subscribe(res => {
       console.log(res);
       alert(JSON.stringify(res))
